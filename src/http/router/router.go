8124f0fafa8ce9dd1076cf4a477c138adecdeb2f
jelena-vlajkov <vlajkovj31@gmail.com>
2021-06-03 05:56:39 +0200

    :bug: Fixed bug with cors policy

--- src/http/router/router.go
+++ src/http/router/router.go
@@ -7,11 +7,12 @@ import (
 
 func NewRouter(handler interactor.AppHandler) *gin.Engine{
 	router := gin.Default()
+	router.Use(CORSMiddleware())
 
 	router.POST("/validateToken", handler.ValidateToken)
 	router.POST("/login", handler.Login)
 	router.POST("/logout", handler.Logout)
-	router.Use(CORSMiddleware())
+
 
 
 	return router
@@ -20,12 +21,14 @@ func NewRouter(handler interactor.AppHandler) *gin.Engine{
 func CORSMiddleware() gin.HandlerFunc {
 	return func(c *gin.Context) {
 
-		c.Header("Access-Control-Allow-Origin", "*")
+		c.Header("Access-Control-Allow-Origin", "http://localhost:4200")
+		c.Header("Access-Control-Allow-Credentials", "true")
 		c.Header("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, accept, origin, Cache-Control, X-Requested-With")
-		c.Header("Access-Control-Allow-Methods", "*")
+		c.Header("Access-Control-Allow-Methods", "POST,HEAD,PATCH, OPTIONS, GET, PUT")
+
 
 		if c.Request.Method == "OPTIONS" {
-			c.AbortWithStatus(204)
+			c.JSON(204, gin.H{})
 			return
 		}
 
