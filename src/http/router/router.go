6908ceab6219025f89367a83cb6381d7c7616e4e
jelena-vlajkov <vlajkovj31@gmail.com>
2021-06-03 04:39:26 +0200

    :bug: Fixed bug with cors policy

--- src/http/router/router.go
+++ src/http/router/router.go
@@ -11,6 +11,24 @@ func NewRouter(handler interactor.AppHandler) *gin.Engine{
 	router.POST("/validateToken", handler.ValidateToken)
 	router.POST("/login", handler.Login)
 	router.POST("/logout", handler.Logout)
+	router.Use(CORSMiddleware())
+
 
 	return router
+}
+
+func CORSMiddleware() gin.HandlerFunc {
+	return func(c *gin.Context) {
+
+		c.Header("Access-Control-Allow-Origin", "*")
+		c.Header("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, accept, origin, Cache-Control, X-Requested-With")
+		c.Header("Access-Control-Allow-Methods", "*")
+
+		if c.Request.Method == "OPTIONS" {
+			c.AbortWithStatus(204)
+			return
+		}
+
+		c.Next()
+	}
 }
