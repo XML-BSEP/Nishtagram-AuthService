695d6fbe986c35ece6ed75d55f407bffbce87c6e
jelena-vlajkov <vlajkovj31@gmail.com>
2021-06-22 01:24:39 +0200

    :bug: fixed bug with paths

--- src/infrastructure/postgresqldb/postgresql_config.go
+++ src/infrastructure/postgresqldb/postgresql_config.go
@@ -14,7 +14,11 @@ func NewDBConnection(logger *logger.Logger) *gorm.DB {
 }
 
 func init_viper(logger *logger.Logger) {
-	viper.SetConfigFile(`src/configurations/postgresql.json`)
+	if os.Getenv("DOCKER_ENV") != "" {
+		viper.SetConfigFile(`src/configurations/postgresql.json`)
+	} else {
+		viper.SetConfigFile(`configurations/postgresql.json`)
+	}
 	err := viper.ReadInConfig()
 	if err != nil {
 		logger.Logger.Infof("error while reading postgresql config file, error: %v\n", err)
